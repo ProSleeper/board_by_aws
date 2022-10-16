7d616d5255b03d13bb248568fba693719b70d61d
kyw <ingngnin@gmail.com>
2022-10-17 01:31:45 +0900

    feat: Posts 클래스와 PostsRepository 클래스 만들고 db와 제대로 연결이 되었는지 PostsRepositoryTest에서 테스트 완료. 성공

---
+++ src/test/java/com/jojoldu/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,52 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import junit.framework.TestCase;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest extends TestCase {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup() {
+
+        postsRepository.deleteAll();
+        System.out.println("애프터 로그 똭!");
+    }
+    
+    @Test
+    public void 게시글저장_불러오기() throws Exception {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("ingn@nate.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+    
+    
+}
